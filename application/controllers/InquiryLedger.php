0b21d025f906bd837e2239bd3bda885d96d41a1f
basiscoding20 <basiscoding20@gmail.com>
2021-08-01 02:59:53 +0700

    Inquiry Selesai

--- application/controllers/InquiryLedger.php
+++ application/controllers/InquiryLedger.php
@@ -22,51 +22,56 @@
 			$this->load->view('plugins/inquiryLedger');
 		}
 
-		// public function getTransaksi()
-		// {
-		// 	$list = $this->InquiryModel->getInquiry();
-		// 	// echo $this->db->last_query($list);
-		// 	$data = array();
-		// 	$no = $_POST['start'];
-
-		// 	foreach ($list as $ls) {
-		// 		$row = array();
-		// 		$row[] = $ls->tgl_voucher;
-		// 		$row[] = $ls->no_voucher;
-		// 		$row[] = $ls->trx_debit;
-		// 		$row[] = $ls->trx_kredit;
-
-		// 		// $row[] = $saldo;
-
-		// 		$data[] = $row;
-		// 	}
-
-		// 	$output = array(
-		// 		"draw" => $_POST['draw'],
-	 //            "recordsTotal" => $this->InquiryModel->count_all(),
-	 //            "recordsFiltered" => $this->InquiryModel->count_filtered(),
-	 //            "data" => $data
-		// 	);
-
-		// 	echo json_encode($output);
-		// }
-
 		public function getTransaksi()
 		{
 			$html = '';
 			$list = $this->InquiryModel->getInquiry();
 			
-			// echo $this->db->last_query($list);
-			foreach ($list as $ls) {
-				$html .= '<tr>
-							<td>'.$ls->tgl_voucher.'</td>
-							<td>'.$ls->trx_description.'</td>
-							<td>'.number_format($ls->trx_debit).'</td>
-							<td>'.number_format($ls->trx_kredit).'</td>
-							<td>'.number_format($ls->saldo).'</td>
-							';
-			}
+			if ($list->num_rows() > 0) {
+				$saldoAwal = $this->InquiryModel->getSaldoAkhir();
 
+					$html .= '<tr>
+								<td colspan="4"></td>
+								<td>'.number_format($saldoAwal).'</td>
+							  </tr>';
+				$count = 1;
+				
+				foreach ($list->result() as $l => $ls) {
+					if ($count==1) {
+						$saldo = $saldoAwal + $ls->trx_debit - $ls->trx_kredit;
+						$debit = $ls->trx_debit;
+						$kredit = $ls->trx_kredit;
+					}else{
+						if ($ls->trx_debit != 0) {
+							$debit = $debit + $ls->trx_debit;
+							$saldo = $saldo + $ls->trx_debit;
+						}else{
+							$kredit = $kredit + $ls->trx_kredit;
+							$saldo = $saldo - $ls->trx_kredit;
+						}
+					}
+					$html .= '<tr>
+								<td>'.$ls->tgl_voucher.'</td>
+								<td>'.$ls->trx_description.'</td>
+								<td>'.number_format($ls->trx_debit).'</td>
+								<td>'.number_format($ls->trx_kredit).'</td>
+								<td>'.number_format($saldo).'</td>
+							  </tr>
+								';
+					$count++;
+				}
+					$html .= '<tr">
+							<td colspan="2"></td>
+							<td class="font-weight-bold">'.number_format($debit).'</td>
+							<td class="font-weight-bold">'.number_format($kredit).'</td>
+							<td class="font-weight-bold">'.number_format($saldo).'</td>
+						  </tr>';
+			}else{
+				$html .= '<tr">
+							<td colspan="5" class="text-center">Tidak Ada Data</td>
+						  </tr>';
+			}
+			
 			echo $html;
 		}
 	
--- application/models/InquiryModel.php
+++ application/models/InquiryModel.php
@@ -3,51 +3,6 @@
 	
 	class InquiryModel extends CI_Model {
 	
-	// 	var $column_order = array(null, 'j.id_jurnal','j.no_voucher','j.description', 'j.tgl_voucher', 'a.no_akun', 'a.sub_no_akun', 'a.nama_akun', 't.trx_debit', 't.trx_kredit', 't.trx_description'); 
-	//     var $column_search = array('j.id_jurnal','j.no_voucher','j.description', 'j.tgl_voucher', 'a.no_akun', 'a.sub_no_akun', 'a.nama_akun', 't.trx_debit', 't.trx_kredit', 't.trx_description'); //field yang diizin untuk pencarian 
-	//     var $order = array('j.tgl_voucher' => 'asc'); // default order 
-	
-	// // Datatable
-	// 	private function _get_datatables_query()
-	// 	{
-	// 		$this->db->select('j.id_jurnal, j.no_voucher,j.description, j.tgl_voucher, a.no_akun, a.sub_no_akun, a.nama_akun, t.trx_debit, t.trx_kredit, t.trx_description');
-	// 		$this->db->from('transaksi_jurnal as t');
- // 			$this->db->join('jurnal as j', 'j.id_jurnal = t.trx_id_jurnal', 'left');
- // 			$this->db->join('account as a', 'a.id = t.trx_id_account', 'left');
- // 			$this->db->where('t.trx_id_account', $this->input->post('id_account'));
- // 			$this->db->where('j.tgl_voucher >=', $this->input->post('startDate'));
-	// 		$this->db->where('j.tgl_voucher <=', $this->input->post('endDate'));
-	//         $i = 0;
-	     	
-	//         foreach ($this->column_search as $item) // looping awal
-	//         {
-	//             if($_POST['search']['value']) // jika datatable mengirimkan pencarian dengan metode POST
-	//             {
-	                 
-	//                 if($i===0) // looping awal
-	//                 {
-	//                     $this->db->group_start();
-	//                     $this->db->like($item, $_POST['search']['value']); 
-	//                 }
-	//                 else
-	//                 {
-	//                     $this->db->or_like($item, $_POST['search']['value']);
-	//                 }
-	 
-	//                 if(count($this->column_search) - 1 == $i) 
-	//                     $this->db->group_end(); 
-	//             }
-	//             $i++;
-	//         }
-	         
-	//         if(isset($_POST['order'])) { // here order processing
-	//             $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
-	//         }  else if(isset($this->order)) {
-	//             $order = $this->order;
-	//             $this->db->order_by(key($order), $order[key($order)]);
-	//         }
-	// 	}
-
 		function getSaldoAwal()
 		{
 			return $this->db->get_where('account', array('id' => $this->input->post('id_account')))->row()->saldo_awal;
@@ -63,6 +18,7 @@
 			$this->db->order_by('j.tgl_voucher', 'asc');
 
 			$query = $this->db->get()->row();
+			// echo $this->db->last_query($query);
 			$saldo_akhir = $query->saldoAkhir;
 
 			if ($saldo_akhir == NULL) {
@@ -75,9 +31,7 @@
 
 		function getInquiry()
 		{
-			$this->db->query('SET @saldo = '.$this->getSaldoAkhir().'');
 			$this->db->select('j.id_jurnal, j.no_voucher,j.description, j.tgl_voucher, a.no_akun, a.sub_no_akun, a.nama_akun, t.trx_debit, t.trx_kredit, t.trx_description');
-			$this->db->select('(@saldo := @saldo + (t.trx_debit-t.trx_kredit)) as saldo');
 			$this->db->from('transaksi_jurnal as t');
  			$this->db->join('jurnal as j', 'j.id_jurnal = t.trx_id_jurnal', 'left');
  			$this->db->join('account as a', 'a.id = t.trx_id_account', 'left');
@@ -85,8 +39,7 @@
  			$this->db->where('j.tgl_voucher >=', $this->input->post('startDate'));
 			$this->db->where('j.tgl_voucher <=', $this->input->post('endDate'));
 			$this->db->order_by('j.tgl_voucher', 'asc');
-			$query = $this->db->get();
-	        return $query->result();
+			return $this->db->get();
 		}
 
 		function count_filtered()
